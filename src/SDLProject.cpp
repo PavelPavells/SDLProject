773ea90fbb5af5efb7b2c869f5a7513eb835380b
Павел Смирнов <Pasha@MacBook-Air-Pavel.local>
2019-11-10 01:19:05 +0300

    [*****]

--- src/SDLProject.cpp
+++ src/SDLProject.cpp
@@ -13,8 +13,6 @@ using namespace std;
 int main(int args, char* argv[]) {
 	cout << "------------------------STARTING PROGRAMM----------------------------" << endl;
 
-	cout << "Hello, Pavel" << endl;
-
 	SDL_Event event;
 	const int SCREEN_HEIGHT = 800;
 	const int SCREEN_WIDTH = 600;
@@ -41,6 +39,66 @@ int main(int args, char* argv[]) {
 		SDL_Quit();
 		return 2;
 	}
+
+	SDL_Renderer *renderer = SDL_CreateRenderer(
+			window,
+			-1,
+			SDL_RENDERER_PRESENTVSYNC
+	);
+
+	SDL_Texture *texture = SDL_CreateTexture(
+			renderer,
+			SDL_PIXELFORMAT_RGBA8888,
+			SDL_TEXTUREACCESS_STATIC,
+			SCREEN_WIDTH,
+			SCREEN_HEIGHT
+	);
+
+	if(renderer == NULL) {
+		cout << "========== Could not create Renderer ==========" << endl;
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		return 3;
+	}
+
+	if(texture == NULL) {
+		cout << "========== Could not create Texture =========" << endl;
+		SDL_DestroyRenderer(renderer);
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		return 4;
+	}
+
+	Uint32 *buffer = new Uint32[SCREEN_WIDTH*SCREEN_HEIGHT];
+
+	memset(
+			buffer,
+			0,
+			SCREEN_WIDTH*SCREEN_HEIGHT*sizeof(Uint32)
+	);
+
+	for(int i = 0; i < SCREEN_WIDTH*SCREEN_HEIGHT; i++) {
+		buffer[i] = 0x0080FFFF;
+	}
+
+	//buffer[30000] = 0xFFFFFFFF;
+
+	SDL_UpdateTexture(
+			texture,
+			NULL,
+			buffer,
+			SCREEN_WIDTH*sizeof(Uint32)
+	);
+
+	SDL_RenderClear(renderer);
+	SDL_RenderCopy(
+			renderer,
+			texture,
+			NULL,
+			NULL
+	);
+	SDL_RenderPresent(renderer);
+
 	while(!quit) {
 			// Update Particles
 			// Draw particles
@@ -54,6 +112,9 @@ int main(int args, char* argv[]) {
 	}
 
 	//SDL_Delay(2000);
+	delete [] buffer;
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyTexture(texture);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
 
